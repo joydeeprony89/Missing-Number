345bc84d9f920e074a98d2d9942a6fd6e1998424
Jaydeep Shil <ED8A@eurofins.com>
2020-10-20 12:40:42 +0530

    Add project files.

---
+++ Missing Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Missing_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 4, 4, 1, 3 };
+            Console.WriteLine(MissingNumber(nums));
+        }
+
+        // https://leetcode.com/problems/missing-number/
+        static int MissingNumber(int[] nums)
+        {
+            int n = nums.Length;
+            int sum = (n * (n + 1)) / 2;
+            foreach (int no in nums) sum -= no;
+            return sum;
+        }
+    }
+}
